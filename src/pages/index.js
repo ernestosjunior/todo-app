f278ed0d8bd5dbcb13e5646eec40493fdd06c107
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-04-15 15:16:39 -0300

    feat: create structure and minimal pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export {default as Splash} from './Splash';
+export {default as SignUp} from './SignUp';
+export {default as SignIn} from './SignIn';
+export {default as Dashboard} from './Dashboard';
---
+++ src/stacks/MainStack.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Splash, SignIn, SignUp, Dashboard} from '../pages';
+
+const Stack = createStackNavigator();
+
+function MainStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Splash" component={Splash} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="Dashboard" component={Dashboard} />
+    </Stack.Navigator>
+  );
+}
+
+export default MainStack;
