af39c437885b961501a62f33fac7ff5ffad27fd3
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-04-15 23:18:15 -0300

    feat: include logic of firstAccess

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BaseLayout } from '../../templates'
 import { HeroSection, Title, Subtitle } from './styles'
 import { Button } from '../../components'
 import Hero from '../../assets/svgs/hero.svg'
 import { useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 function Splash() {
   const { navigate } = useNavigation()
+
+  useEffect(() => {
+    async function isFirstAccess() {
+      const storage = await AsyncStorage.getItem('previouslyAccessed')
+
+      if (storage == null) return false
+
+      const value = Boolean(storage)
+
+      if (value) return navigate('SignIn')
+
+      return false
+    }
+    isFirstAccess()
+  }, [])
+
   return (
     <BaseLayout>
       <HeroSection>
@@ -17,7 +34,13 @@ function Splash() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum dictum
         tempus, interdum at dignissim metus. Ultricies sed nunc.
       </Subtitle>
-      <Button label="Get Started" onClick={() => navigate('SignUp')} />
+      <Button
+        label="Get Started"
+        onClick={async () => {
+          await AsyncStorage.setItem('previouslyAccessed', '1')
+          navigate('SignUp')
+        }}
+      />
     </BaseLayout>
   )
 }
